91697a0ba2d0f4fb60a45e06537465678f042a4e
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-11 18:42:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     name_hash.collect do |key, value|
         if lowest > value
             lowest = value
-            puts lowest
+            key
         end
     end
 end
